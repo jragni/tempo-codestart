e20abfa5b7e17e7b0fdc9b23f4bb2e72f3d80699
Jhensen Ray Agni <jhensenrayagni@gmaill.com>
2024-07-15 10:46:12 -0700

    fix proptype error for build

--- app/problems/[problemTitle]/page.tsx
+++ app/problems/[problemTitle]/page.tsx
@@ -4,7 +4,12 @@
 
 import { Workspace } from "@containers"
 
-export default function ProblemsPage({ params }) {
+interface PageProps {
+  params?: { slug: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+export default function ProblemsPage({ params }: PageProps) {
   const { problemTitle } = params;
 
   const files = {
@@ -15,19 +20,22 @@ export default function ProblemsPage({ params }) {
       code: `
 test('myFunction should log a message', () => {
   const consoleLogSpy = jest.spyOn(console, 'log');
-  expect(consoleLogSpy).toHaveBeenCalledWith('Hello, World!');
+  expect(consoleLogSpy).toHaveBeenCalledWith("Hello, World!");
   consoleLogSpy.mockRestore(); // Clean up the spy after the test
 });`,
-      readOnly: true,
-      hidden: true,
     }
   }
 
   return (
     <div className="flex flex-col justify-between w-full font-bold bg-base-300">
-      <div className="p-4">
-        <h3 className="text-3xl mb-10">Your First Problem</h3>
-        <p className="font-thin">Welcome to Tempo! for your first problem console</p>
+      <div className="p-4 flex flex-row justify-between">
+        <div>
+          <h3 className="text-3xl mb-10">Your First Problem</h3>
+          <p className="font-thin">Welcome to Tempo! for your first problem console</p>
+        </div>
+        <div>
+
+        </div>
       </div>
       <Workspace files={files} />
     </div>
