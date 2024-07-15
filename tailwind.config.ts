94b07e3926a19795aa4cfacee1ef311a7b5aa6ee
Jhensen Ray Agni <jhensenrayagni@gmaill.com>
2024-07-14 17:49:07 -0700

    update gitignore

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,17 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+   // daisyUI config (optional - here are the default values)
+   daisyui: {
+    themes: ["synthwave"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +26,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
 export default config;
